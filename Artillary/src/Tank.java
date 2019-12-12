4df542725fd01f8873e9820c4fa1fa1b2342c0b3
Mats Bos <matsbos2001@hotmail.com>
2019-12-12 13:44:46 +0100

    lel

--- Artillary/src/Tank.java
+++ Artillary/src/Tank.java
@@ -4,11 +4,17 @@ public class Tank {
     private int tank2Health = 100;
     private Random rnd = new Random();
     private String playerName;
+    private double angle;
+    private double velocity;
+    private int xPositie;
+    private int yYositie;
 
 
-    public Tank(String playerName)
+    public Tank(String playerName, int xPositie, int yYositie)
     {
         this.playerName = playerName;
+        this.xPositie = xPositie;
+        this.yYositie = yYositie;
     }
 
     public String dealDamage(String target)
@@ -22,6 +28,24 @@ public class Tank {
         }
         return String.format("%d damage dealt to %s %n%s has %d health left", damage, target, target, tank2Health);
     }
+
+    public int getxPositie() {
+        return xPositie;
+    }
+
+    public int getyYositie() {
+        return yYositie;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public void setVelocity(double velocity) {
+        this.velocity = velocity;
+    }
+
+
 
     public int getTank2Health() {
         return tank2Health;
--- Artillary/src/TestTank.java
+++ Artillary/src/TestTank.java
@@ -3,21 +3,27 @@ import java.util.Scanner;
 
 public class TestTank {
     public static void main(String[] args) {
-        Tank t1 = new Tank("Tank 1");
+        Tank t1 = new Tank("Tank 1",20,20);
 //        System.out.println(t1.dealDamage());
 
         while(t1.getTank2Health() != 0) {
-            System.out.println("Make a move:");
+            System.out.println("Make a move(dealDamage):");
             Scanner keyboard = new Scanner(System.in);
-            String input = keyboard.nextLine();
+            String input = keyboard.nextLine().toLowerCase();
+            System.out.printf("your enemy is on coordinates x:%d, y:%d \n",t1.getxPositie(), t1.getyYositie());
 
-
-            if (input.equals("dealDamage")) {
-                System.out.println("Choose a target(choose name)");
+            if (input.equals("dealdamage")) {
+                System.out.println("Choose enemy (name): ");
                 String target = keyboard.nextLine();
+                System.out.println("Choose a angle: ");
+                int angle = keyboard.nextInt();
+                t1.setAngle(angle);
+                System.out.println("Choose a velocity: ");
+                int vel = keyboard.nextInt();
+                t1.setAngle(vel);
                 System.out.println(t1.dealDamage(target));
             } else {
-                while (!input.equals("dealDamage")) {
+                while (!input.equals("dealdamage")) {
                     System.out.println("Not a valid move, the current moves are:");
                     System.out.println("dealDamage");
                     input = keyboard.nextLine();
